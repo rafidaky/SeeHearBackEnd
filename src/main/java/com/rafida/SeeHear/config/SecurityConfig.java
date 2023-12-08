952c7a397386dd7c7667ecc8ee23d33ea29d901d
rafidaky <rafidakn@gmail.com>
2023-12-08 21:30:53 +0100

    401 fix for register

--- src/main/java/com/rafida/SeeHear/config/SecurityConfig.java
+++ src/main/java/com/rafida/SeeHear/config/SecurityConfig.java
@@ -2,14 +2,29 @@ package com.rafida.SeeHear.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
-public class SecurityConfig {
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/api/users/register").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .disable();
+    }
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 }
-
--- src/main/java/com/rafida/SeeHear/model/User.java
+++ src/main/java/com/rafida/SeeHear/model/User.java
@@ -3,6 +3,8 @@ package com.rafida.SeeHear.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+
+//instances of this class will be stored in a MongoDB collection named "users"
 @Document(collection = "users")
 public class User {
     @Id
@@ -13,16 +15,19 @@ public class User {
     // Constructors, getters, setters
 
     // Constructors
+    // initializes an object of the User class with default values
     public User() {
         // Default constructor
     }
-
+    //A parameterized constructor (public User(String username, String password))
+    //that allows you to create a User object with specified values for username and password.
     public User(String username, String password) {
         this.username = username;
         this.password = password;
     }
 
     // Getters and Setters
+    //These methods are getter and setter methods that allow you to retrieve and modify the values of the private fields (id, username, password).
     public String getId() {
         return id;
     }
